cfaf837e3a38752e452056b54a7b6d70a6b05ac0
Paul Stearns <celestialcoyote@gmail.com>
2023-03-29 17:30:15 -0700

    Added Header component

--- src/app/head.js
+++ src/app/head.js
@@ -2,7 +2,7 @@ export default function Head() {
 	return (
 		<>
 			<title>IMDB Clone</title>
-			<meta content='width=device-width, initial-scale-1' name='viewport' />
+			<meta content='width=device-width, initial-scale=1' name='viewport' />
 			<meta name='description' content='IMDB Clone' />
 			<link rel='icon' href='/favicon.ico' />
 		</>
