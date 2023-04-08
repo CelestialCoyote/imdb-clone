40f7ef20c62a8ce686550dfe0420bc26358a146b
Paul Stearns <celestialcoyote@gmail.com>
2023-04-08 16:14:51 -0700

    created NavBar and NavBarItem components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import NavBar from '@/components/NavBar';
 import Providers from './Providers';
 import './globals.css';
 
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
 			<body>
 				<Providers>
 					<Header />
+					<NavBar />
 					{children}
 				</Providers>
 			</body>
